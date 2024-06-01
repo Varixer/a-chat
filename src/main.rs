aaad9b4a254480c9ffde713ecdaf75feaa54528f
Varixer <penglei.x.mao@gmail.com>
2024-06-01 15:17:58 +0800

    🦄 refactor: Done

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
+mod client;
 mod server;
-fn main() {
-    println!("Hello, world!");
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+fn main() -> Result<()> {
+    let mut args = std::env::args();
+    match (args.nth(1).as_ref().map(String::as_str), args.next()) {
+        (Some("client"), None) => client::main(),
+        (Some("server"), None) => server::main(),
+        _ => Err("Usage: a-chat [client|server]".into()),
+    }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,45 +1,66 @@
+use std::{
+    collections::hash_map::{Entry, HashMap},
+    sync::Arc,
+};
+
+use futures::{channel::mpsc, select, FutureExt, SinkExt};
+
 use async_std::{
     io::BufReader,
     net::{TcpListener, TcpStream, ToSocketAddrs},
     prelude::*,
     task,
 };
-use futures::channel::mpsc;
-use futures::sink::SinkExt;
-use std::collections::hash_map::{Entry, HashMap};
-use std::sync::Arc;
 
-type Sender<T> = mpsc::UnboundedSender<T>; // 2
-type Receiver<T> = mpsc::UnboundedReceiver<T>;
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+type Sender<T> = mpsc::UnboundedSender<T>;
+type Receiver<T> = mpsc::UnboundedReceiver<T>;
+
+#[derive(Debug)]
+enum Void {}
+
+pub(crate) fn main() -> Result<()> {
+    task::block_on(accept_loop("127.0.0.1:8080"))
+}
 
 async fn accept_loop(addr: impl ToSocketAddrs) -> Result<()> {
     let listener = TcpListener::bind(addr).await?;
-    let mut incomming = listener.incoming();
-    while let Some(stream) = incomming.next().await {
-        // 我们使用 task::spawn function 来生成一个独立的任务，用于与每个客户端一起工作。
-        // 也就是说，在接受客户端后， accept_loop 立即开始等待下一个客户端。
-        // 这是事件驱动架构的核心优势：我们同时为许多客户提供服务，而无需花费很多硬件线程。
+
+    let (broker_sender, broker_receiver) = mpsc::unbounded();
+    let broker = task::spawn(broker_loop(broker_receiver));
+    let mut incoming = listener.incoming();
+    while let Some(stream) = incoming.next().await {
         let stream = stream?;
-        println!("Accepting from : {}", stream.peer_addr()?);
-        spawn_and_log_error(connection_loop(stream));
+        println!("Accepting from: {}", stream.peer_addr()?);
+        spawn_and_log_error(connection_loop(broker_sender.clone(), stream));
     }
+    drop(broker_sender);
+    broker.await;
     Ok(())
 }
 
-async fn connection_loop(stream: TcpStream) -> Result<()> {
-    let reader = BufReader::new(&stream);
+async fn connection_loop(mut broker: Sender<Event>, stream: TcpStream) -> Result<()> {
+    let stream = Arc::new(stream);
+    let reader = BufReader::new(&*stream);
     let mut lines = reader.lines();
 
     let name = match lines.next().await {
-        None => Err("peer disconnected immediately")?,
+        None => return Err("peer disconnected immediately".into()),
         Some(line) => line?,
     };
-    println!("name = {}", name);
+    let (_shutdown_sender, shutdown_receiver) = mpsc::unbounded::<Void>();
+    broker
+        .send(Event::NewPeer {
+            name: name.clone(),
+            stream: Arc::clone(&stream),
+            shutdown: shutdown_receiver,
+        })
+        .await
+        .unwrap();
 
     while let Some(line) = lines.next().await {
         let line = line?;
-        let (dest, msg) = match line.find(":") {
+        let (dest, msg) = match line.find(':') {
             None => continue,
             Some(idx) => (&line[..idx], line[idx + 1..].trim()),
         };
@@ -47,18 +68,38 @@ async fn connection_loop(stream: TcpStream) -> Result<()> {
             .split(',')
             .map(|name| name.trim().to_string())
             .collect();
-        let msg: String = msg.to_string();
+        let msg: String = msg.trim().to_string();
+
+        broker
+            .send(Event::Message {
+                from: name.clone(),
+                to: dest,
+                msg,
+            })
+            .await
+            .unwrap();
     }
+
     Ok(())
 }
 
 async fn connection_writer_loop(
-    mut messages: Receiver<String>,
+    messages: &mut Receiver<String>,
     stream: Arc<TcpStream>,
+    mut shutdown: Receiver<Void>,
 ) -> Result<()> {
     let mut stream = &*stream;
-    while let Some(msg) = messages.next().await {
-        stream.write_all(msg.as_bytes()).await?;
+    loop {
+        select! {
+            msg = messages.next().fuse() => match msg {
+                Some(msg) => stream.write_all(msg.as_bytes()).await?,
+                None => break,
+            },
+            void = shutdown.next().fuse() => match void {
+                Some(void) => match void {},
+                None => break,
+            }
+        }
     }
     Ok(())
 }
@@ -68,6 +109,7 @@ enum Event {
     NewPeer {
         name: String,
         stream: Arc<TcpStream>,
+        shutdown: Receiver<Void>,
     },
     Message {
         from: String,
@@ -76,46 +118,67 @@ enum Event {
     },
 }
 
-async fn broker_loop(mut events: Receiver<Event>) -> Result<()> {
+async fn broker_loop(mut events: Receiver<Event>) {
+    let (disconnect_sender, mut disconnect_receiver) =
+        mpsc::unbounded::<(String, Receiver<String>)>();
     let mut peers: HashMap<String, Sender<String>> = HashMap::new();
 
-    while let Some(event) = events.next().await {
-        match event {
-            Event::NewPeer { name, stream } => {
-                match peers.entry(name) {
-                    Entry::Occupied(_) => (),
-                    Entry::Vacant(entry) => {
-                        let (client_sender, client_receiver) = mpsc::unbounded();
-                        entry.insert(client_sender);
-                        spawn_and_log_error(connection_writer_loop(client_receiver, stream));
-                    },
-                }
+    loop {
+        let event = select! {
+            event = events.next().fuse() => match event {
+                None => break,
+                Some(event) => event,
             },
+            disconnect = disconnect_receiver.next().fuse() => {
+                let (name, _pending_messages) = disconnect.unwrap();
+                assert!(peers.remove(&name).is_some());
+                continue;
+            },
+        };
+        match event {
             Event::Message { from, to, msg } => {
                 for addr in to {
                     if let Some(peer) = peers.get_mut(&addr) {
                         let msg = format!("from {}: {}\n", from, msg);
-                        peer.send(msg).await?
+                        peer.send(msg).await.unwrap();
                     }
                 }
             }
+            Event::NewPeer {
+                name,
+                stream,
+                shutdown,
+            } => match peers.entry(name.clone()) {
+                Entry::Occupied(..) => (),
+                Entry::Vacant(entry) => {
+                    let (client_sender, mut client_receiver) = mpsc::unbounded();
+                    entry.insert(client_sender);
+                    let mut disconnect_sender = disconnect_sender.clone();
+                    spawn_and_log_error(async move {
+                        let res =
+                            connection_writer_loop(&mut client_receiver, stream, shutdown).await;
+                        disconnect_sender
+                            .send((name, client_receiver))
+                            .await
+                            .unwrap();
+                        res
+                    });
+                }
+            },
         }
     }
-    Ok(())
+    drop(peers);
+    drop(disconnect_sender);
+    while let Some((_name, _pending_messages)) = disconnect_receiver.next().await {}
 }
 
 fn spawn_and_log_error<F>(fut: F) -> task::JoinHandle<()>
 where
-    F: Future<Output = Result<()>> + Send + 'static
+    F: Future<Output = Result<()>> + Send + 'static,
 {
     task::spawn(async move {
         if let Err(e) = fut.await {
-            eprintln!("error: {}", e);
+            eprintln!("{}", e)
         }
     })
-}
-
-fn run() -> Result<()> {
-    let fut = accept_loop("127.0.0.1:8080");
-    task::block_on(fut)
 }
