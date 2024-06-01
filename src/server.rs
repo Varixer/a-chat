b44cd8558409eb01a60eb21f6161b980cb571c14
Varixer <penglei.x.mao@gmail.com>
2024-06-01 14:29:16 +0800

    ✨ feat(Server): Connect Readers and Writers

--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,19 @@
 use async_std::{
-    io::BufReader, net::{TcpListener, TcpStream, ToSocketAddrs}, prelude::*, task
+    io::BufReader,
+    net::{TcpListener, TcpStream, ToSocketAddrs},
+    prelude::*,
+    task,
 };
+use futures::channel::mpsc;
+use futures::sink::SinkExt;
+use std::collections::hash_map::{Entry, HashMap};
+use std::sync::Arc;
 
+type Sender<T> = mpsc::UnboundedSender<T>; // 2
+type Receiver<T> = mpsc::UnboundedReceiver<T>;
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 async fn accept_loop(addr: impl ToSocketAddrs) -> Result<()> {
-    
     let listener = TcpListener::bind(addr).await?;
     let mut incomming = listener.incoming();
     while let Some(stream) = incomming.next().await {
@@ -14,8 +22,7 @@ async fn accept_loop(addr: impl ToSocketAddrs) -> Result<()> {
         // 这是事件驱动架构的核心优势：我们同时为许多客户提供服务，而无需花费很多硬件线程。
         let stream = stream?;
         println!("Accepting from : {}", stream.peer_addr()?);
-        let handle = task::spawn(connection_loop(stream));
-        handle.await?
+        spawn_and_log_error(connection_loop(stream));
     }
     Ok(())
 }
@@ -31,20 +38,75 @@ async fn connection_loop(stream: TcpStream) -> Result<()> {
     println!("name = {}", name);
 
     while let Some(line) = lines.next().await {
-        let line  = line?;
+        let line = line?;
         let (dest, msg) = match line.find(":") {
             None => continue,
-            Some(idx) => (&line[..idx], line[idx + 1 ..].trim()),
+            Some(idx) => (&line[..idx], line[idx + 1..].trim()),
         };
-        let dest: Vec<String> = dest.split(',').map(|name| name.trim().to_string()).collect();
+        let dest: Vec<String> = dest
+            .split(',')
+            .map(|name| name.trim().to_string())
+            .collect();
         let msg: String = msg.to_string();
+    }
+    Ok(())
+}
+
+async fn connection_writer_loop(
+    mut messages: Receiver<String>,
+    stream: Arc<TcpStream>,
+) -> Result<()> {
+    let mut stream = &*stream;
+    while let Some(msg) = messages.next().await {
+        stream.write_all(msg.as_bytes()).await?;
+    }
+    Ok(())
+}
+
+#[derive(Debug)]
+enum Event {
+    NewPeer {
+        name: String,
+        stream: Arc<TcpStream>,
+    },
+    Message {
+        from: String,
+        to: Vec<String>,
+        msg: String,
+    },
+}
+
+async fn broker_loop(mut events: Receiver<Event>) -> Result<()> {
+    let mut peers: HashMap<String, Sender<String>> = HashMap::new();
+
+    while let Some(event) = events.next().await {
+        match event {
+            Event::NewPeer { name, stream } => {
+                match peers.entry(name) {
+                    Entry::Occupied(_) => (),
+                    Entry::Vacant(entry) => {
+                        let (client_sender, client_receiver) = mpsc::unbounded();
+                        entry.insert(client_sender);
+                        spawn_and_log_error(connection_writer_loop(client_receiver, stream));
+                    },
+                }
+            },
+            Event::Message { from, to, msg } => {
+                for addr in to {
+                    if let Some(peer) = peers.get_mut(&addr) {
+                        let msg = format!("from {}: {}\n", from, msg);
+                        peer.send(msg).await?
+                    }
+                }
+            }
+        }
     }
     Ok(())
 }
 
 fn spawn_and_log_error<F>(fut: F) -> task::JoinHandle<()>
 where
-    F: Future<Output = Result<()>> + Send +'static,
+    F: Future<Output = Result<()>> + Send + 'static
 {
     task::spawn(async move {
         if let Err(e) = fut.await {
